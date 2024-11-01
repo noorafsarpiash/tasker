b833e800ae2c8a10e37ae650de088cd407ddfe59
Noor Afsar Piash <74808252+noorpiash@users.noreply.github.com>
2024-11-01 22:27:48 +0600

    done

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -16,22 +16,86 @@ export default function TaskBoard() {
   };
   const [tasks, setTasks] = useState([defaultTask]);
   const [showAddModal, setShowAddModal] = useState(false);
+  const [taskToUpdate, setTaskToUpdate] = useState(null);
 
-  function handleAddTask(task) {
-    console.log("Task Added", task);
+  function handleAddEditTask(newTask, isAdd) {
+    if (isAdd) {
+      setTasks([...tasks, newTask]);
+    } else {
+      setTasks(
+        tasks.map((task) => {
+          if (task.id === newTask.id) {
+            return newTask;
+          }
+          return task;
+        })
+      );
+    }
+
+    setShowAddModal(false);
+  }
+
+  function handleEditTask(task) {
+    setTaskToUpdate(task);
+    setShowAddModal(true);
+  }
+
+  function onCloseClick() {
+    setShowAddModal(false);
+    setTaskToUpdate(null);
+  }
+
+  function handleTaskDelete(taskId) {
+    const taskAfterDelete = tasks.filter((task) => task.id !== taskId);
+    setTasks(taskAfterDelete);
+  }
+
+  function handleDeleteAllClick() {
+    tasks.length = 0;
+    setTasks([...tasks]);
+  }
+
+  function handleFavorite(taskId) {
+    const taskIndex = tasks.findIndex((task) => task.id === taskId);
+
+    const newTasks = [...tasks];
+    newTasks[taskIndex].isFavorite = !newTasks[taskIndex].isFavorite;
+    setTasks(newTasks);
+  }
+
+  function handleSearch(searchTerm) {
+    console.log(searchTerm);
+    const filtered = tasks.filter((task) =>
+      task.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setTasks([...filtered]);
   }
 
   return (
     <section className="mb-20" id="tasks">
-      {showAddModal && <AddTaskModal onSave={handleAddTask} />}
+      {showAddModal && (
+        <AddTaskModal
+          onSave={handleAddEditTask}
+          taskToUpdate={taskToUpdate}
+          onCloseClick={onCloseClick}
+        />
+      )}
       <div className="container">
         <div className="p-2 flex justify-end">
-          <SearchTask></SearchTask>
+          <SearchTask onSearch={handleSearch}></SearchTask>
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions onAddClick={() => setShowAddModal(true)}></TaskActions>
-          <TaskList tasks={tasks}></TaskList>
+          <TaskActions
+            onAddClick={() => setShowAddModal(true)}
+            onDeleteAllClick={handleDeleteAllClick}
+          ></TaskActions>
+          <TaskList
+            tasks={tasks}
+            onEdit={handleEditTask}
+            onDelete={handleTaskDelete}
+            onFav={handleFavorite}
+          ></TaskList>
         </div>
       </div>
     </section>
